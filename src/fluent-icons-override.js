9bf14a3f665bbffa962da5f0fb323958affd8b62
Ayesh Nipun <ayesh@ascentic.se>
2025-11-08 15:06:03 +0530

    Icon changes

---
+++ src/fluent-icons-override.js
@@ -0,0 +1,4 @@
+import { initializeIcons } from '@fluentui/font-icons-mdl2';
+
+// Option 1: Use local copy (copied via webpack plugin)
+initializeIcons('/fabric-font/', { disableWarnings: true });
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,20 @@
+import './fluent-icons-override';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { initializeIcons } from '@fluentui/react';
+// import { initializeIcons } from '@fluentui/react/lib/Icons';
+// import { initializeIcons } from '@fluentui/font-icons-mdl2';
+
+// initializeIcons('/fabric-font/', { disableWarnings: true });
 
 // const selfOpen = window.open;
 // (window.open as any) = function(url: any, windowName: any, parms: any) {
 //   selfOpen(url,"_self",parms || "")
 // };
-initializeIcons("https://spoppe-b.azureedge.net/files/fabric-cdn-prod_20210407.001/assets/icons/");
+// initializeIcons("https://spoppe-b.azureedge.net/files/fabric-cdn-prod_20210407.001/assets/icons/");
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
